4616aadd0a9feb9c4c501aa5dc6d5178b19927ce
njorogedk <njoroge.info@gmail.com>
2021-02-22 16:03:23 -0600

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKjhp0tdS6zNQWpOaLDQnhGm5OaLAwwk0",
+    authDomain: "slack-clone-b2ca7.firebaseapp.com",
+    projectId: "slack-clone-b2ca7",
+    storageBucket: "slack-clone-b2ca7.appspot.com",
+    messagingSenderId: "701240719608",
+    appId: "1:701240719608:web:ed1d627f119753dc6f230d"
+  };
